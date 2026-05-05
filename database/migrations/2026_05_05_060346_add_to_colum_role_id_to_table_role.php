b6290f974f24a43bca5810586088a5b83aaa98f0
Fillip Dev <developer@filliptechnologies.com>
2026-05-05 11:38:27 +0530

    add more colums

--- database/migrations/2026_05_05_060346_add_to_colum_role_id_to_table_role.php
+++ database/migrations/2026_05_05_060346_add_to_colum_role_id_to_table_role.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->enum('role',['admin','employee','hr','intern']);
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
--- database/migrations/2026_05_05_060601_add_to_colum_designation_to_table_role.php
+++ database/migrations/2026_05_05_060601_add_to_colum_designation_to_table_role.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->enum('role',['admin','employee','hr','intern']);
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('designation')->after('profile');
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
